b72d77423eced5c1d86a855a9d2e6b82c9bd53b9
Fabián Gtz <fabian.gutierrez.gomez@gmail.com>
2021-04-25 01:48:05 -0500

    More Tilsets and code

--- Inframundo/Scenes/Heroe.gd
+++ Inframundo/Scenes/Heroe.gd
@@ -1,18 +1,33 @@
-extends Sprite
+extends Sprite 
+var can_move
 
+#Vectores para el personaje
 var dir = {
-	"Right": Vector2(40,20),
-	"Down": Vector2(-40,20),
-	"Up": Vector2(40,-20),
-	"Left": Vector2(-40,-20)
+	"Down": Vector2(40,20),
+	"Left": Vector2(-40,20),
+	"Right": Vector2(40,-20),
+	"Up": Vector2(-40,-20)
 }
-
-func _physics_process(delta):
-	if Input.is_action_just_pressed("ui_up"):
+# Para el movimiento del personaje
+func _physics_process(delta): 
+	if Input.is_action_pressed("ui_up"):
 		move_to(dir.Up)
+	if Input.is_action_pressed("ui_down"):
+		move_to(dir.Down)
+	if Input.is_action_pressed("ui_right"):
+		move_to(dir.Right)
+	if Input.is_action_pressed("ui_left"):
+		move_to(dir.Left)
 		
+		
+		
+#funcion de la animacion del personaje		
 func move_to(dir):
+	can_move = false
 	$Tween.interpolate_property(self, "position",
 					position, position + dir, 0.5,
 					Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	$Tween.start()
+	yield($Tween, "tween_completed")
+	can_move = true
+	

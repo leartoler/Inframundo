d831e213edc44c99c062e8558064036b66d8ec85
Fabián Gtz <fabian.gutierrez.gomez@gmail.com>
2021-04-24 20:39:11 -0500

    Tileset

--- Inframundo/Scenes/Heroe.gd
+++ Inframundo/Scenes/Heroe.gd
@@ -1,16 +1,18 @@
 extends Sprite
 
+var dir = {
+	"Right": Vector2(40,20),
+	"Down": Vector2(-40,20),
+	"Up": Vector2(40,-20),
+	"Left": Vector2(-40,-20)
+}
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_up"):
+		move_to(dir.Up)
+		
+func move_to(dir):
+	$Tween.interpolate_property(self, "position",
+					position, position + dir, 0.5,
+					Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.start()

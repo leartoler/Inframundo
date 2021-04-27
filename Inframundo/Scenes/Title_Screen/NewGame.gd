e3c95f7e7b72355a608b58c2094b65c718c0ad78
leartoler <fabiangg@protonmail.ch>
2021-04-26 20:39:36 -0500

    Menu inicial terminado

---
+++ Inframundo/Scenes/Title_Screen/NewGame.gd
@@ -0,0 +1,22 @@
+extends Control
+
+var scene_path_to_load
+#var scene_to_load
+
+func _on_NewGameButton_pressed():
+	get_tree().change_scene("res://Scenes/Lvl/Lvl_01.tscn")
+func _on_ExitButton_pressed():
+	get_tree().quit()
+
+#Lo de abajo es para hacer que se dispare un fade a la hora de darle un click a ago
+
+#func _on_Button_pressed(scene_to_load):
+#	scene_path_to_load = scene_to_load
+#	$FadeIn.show()
+#	$FadeIn.fade_in()
+#
+#func _on_FadeIn_fade_finished():
+#	get_tree().change_scene("scene_path_to_load")
+	
+
+

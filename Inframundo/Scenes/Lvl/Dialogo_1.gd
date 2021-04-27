4846c94e7f492ff20054bc5207f4517e32cc319e
leartoler <fabiangg@protonmail.ch>
2021-04-26 20:03:34 -0500

    Ay Diosito, plis

---
+++ Inframundo/Scenes/Lvl/Dialogo_1.gd
@@ -0,0 +1,17 @@
+extends Area2D
+export(int) var dialog = 0
+
+
+
+func _on_Dialogo_1_body_entered(body):
+	if body.is_in_group("arbol"):
+		body.dialog_id = dialog
+
+
+func _on_Dialogo_1_body_exited(body):
+	if body.is_in_group("arbol"):
+		body.dialog_id = null
+
+func animation():
+	$anim.play("Dialogo")
+

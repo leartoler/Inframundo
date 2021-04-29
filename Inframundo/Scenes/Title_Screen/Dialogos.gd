17c4eca1693e1650f21c7bd8fe973476190fc3bb
Fabián Gtz <fabian.gutierrez.gomez@gmail.com>
2021-04-29 12:30:47 -0500

    cambios pos crisis

---
+++ Inframundo/Scenes/Title_Screen/Dialogos.gd
@@ -0,0 +1,14 @@
+extends Area2D
+export(int) var dialog = 0
+
+
+
+
+func _on_Dialogo_1_body_entered(body):
+	if body.is_in_group("arbol"):
+		body.dialog_id = dialog
+
+func _on_Dialogo_1_body_exited(body):
+	if body.is_in_group("arbol"):
+		body.dialog_id = null
+		

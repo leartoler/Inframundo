3612b5b643b26606c93f08c657a379d8eba57a76
Fabián Gtz <fabian.gutierrez.gomez@gmail.com>
2021-05-08 05:28:17 -0500

    Movimiento del heroe
    Movimiento del Heroe en Idle

--- Inframundo/Script/Heroe.gd
+++ Inframundo/Script/Heroe.gd
@@ -2,6 +2,7 @@ extends Sprite
 var can_move = true
 var dialog = null 
 
+
 #Vectores para el personaje
 var dir = {
 	"Down": Vector2(40,20),
@@ -9,9 +10,12 @@ var dir = {
 	"Right": Vector2(40,-20),
 	"Up": Vector2(-40,-20)
 }
+
+
 # Para el movimiento del personaje
 # Raycast que permite mover si, y siempre si, no choque con algo.
 func _physics_process(delta): 
+	#$anim.play("Toggle")
 	if can_move:
 		if Input.is_action_pressed("ui_up") and !$Up.is_colliding():
 			move_to(dir.Up)
@@ -21,6 +25,11 @@ func _physics_process(delta):
 			move_to(dir.Right)
 		elif Input.is_action_pressed("ui_left") and !$Left.is_colliding():
 			move_to(dir.Left)
+		
+		
+
+#func toggle():
+#	$anim.play("Toggle")	
 		
 #Movimiento según donde se mueva el prota
 func move_to(direction):
@@ -34,7 +43,10 @@ func move_to(direction):
 			set_frame(1)
 		dir.Left:
 			set_frame(3)
-			
+
+
+	
+	
 	can_move =  false
 	$Tween.interpolate_property(self, "global_position",
 					global_position, global_position + direction, 0.5,
@@ -42,6 +54,7 @@ func move_to(direction):
 	$Tween.start()
 	yield($Tween, "tween_completed")
 	can_move = true
+
 	
 	#Diálogo
 	if dialog != null and Input.is_action_just_pressed("ui_accept"):

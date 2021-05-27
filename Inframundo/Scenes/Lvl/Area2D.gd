8874152b77c0acd39d48cb9ae242b20075a67bf3
Fabián Gtz <fabian.gutierrez.gomez@gmail.com>
2021-05-27 13:13:07 -0500

    cambios minimos

--- Inframundo/Scenes/Lvl/Area2D.gd
+++ Inframundo/Scenes/Lvl/Area2D.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("arbol"):
-		get_tree().change_scene("res://Scenes/Title_Screen/Final.tscn")
+		get_tree().change_scene("res://Scenes/Lvl/Lvl_02.tscn")
 
 
 func _on_Area2D_body_exited(body):

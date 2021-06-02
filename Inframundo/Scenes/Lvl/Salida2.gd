d7a7a6a43b2c614424e470938c42122232871763
Fabián Gtz <fabian.gutierrez.gomez@gmail.com>
2021-06-02 12:28:21 -0500

    Cambios en el nivel 2, ya finalizado

--- Inframundo/Scenes/Lvl/Salida2.gd
+++ Inframundo/Scenes/Lvl/Salida2.gd
@@ -1,17 +1,2 @@
-extends Area2D
-
-func _on_Salida_body_exited(body):
-	if body.is_in_group("arbol"):
-		get_tree().change_scene("res://Scenes/Lvl/Lvl_03.tscn")
-
-
-func _on_Salida_body_entered(body):
-	if body.is_in_group("arbol"):
-		body.dialog_id = null
-
-
-
-func animation():
-	$anim.play("Dialogo")
 
 
--- Inframundo/Scenes/Title_Screen/Dialogos.gd
+++ Inframundo/Scenes/Title_Screen/Dialogos.gd
@@ -11,4 +11,5 @@ func _on_Dialogo_1_body_entered(body):
 func _on_Dialogo_1_body_exited(body):
 	if body.is_in_group("arbol"):
 		body.dialog_id = null
+		queue_free()
 		

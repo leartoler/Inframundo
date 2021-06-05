5f7ad2725914d2038c6cec59670ec48c57190ff2
leartoler <fabiangg@protonmail.ch>
2021-06-05 13:50:38 -0500

    si si, la chingada

--- Inframundo/Scenes/Title_Screen/Dialogos.gd
+++ Inframundo/Scenes/Title_Screen/Dialogos.gd
@@ -7,8 +7,11 @@ export(int) var dialog = 0
 func _on_Dialogo_1_body_entered(body):
 	if body.is_in_group("arbol"):
 		body.dialog_id = dialog
+	
+		
 
 func _on_Dialogo_1_body_exited(body):
 	if body.is_in_group("arbol"):
-		body.dialog_id = null
+		#body.dialog_id = null
+		queue_free()
 		

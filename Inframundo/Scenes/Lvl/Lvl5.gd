8798e5cc50bb6651ea0319afb8b0fcda5b97db3f
leartoler <fabiangg@protonmail.ch>
2021-06-06 17:11:57 -0500

    Ya casi todos los finales terminados.

---
+++ Inframundo/Scenes/Lvl/Lvl5.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+func _on_Lvl5_body_entered(body):
+	if body.is_in_group("arbol"):
+		get_tree().change_scene("res://Scenes/Title_Screen/Final.tscn")
+
+
+func _on_Lvl5_body_exited(body):
+	if body.is_in_group("arbol"):
+		body.dialog_id = null
+
+func animation():
+	$anim.play("Dialogo")
--- Inframundo/Scenes/Title_Screen/NewGame.gd
+++ Inframundo/Scenes/Title_Screen/NewGame.gd
@@ -4,7 +4,7 @@ var scene_path_to_load
 #var scene_to_load
 
 func _on_NewGameButton_pressed():
-	get_tree().change_scene("res://Scenes/Lvl/Lvl_01.tscn")
+	get_tree().change_scene("res://Scenes/Title_Screen/TitleScreen.tscn")
 func _on_ExitButton_pressed():
 	get_tree().quit()
 
